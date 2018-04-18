3b7c24482755e5d3f0e7bdbf96f4b98094283633
Coty Condry <ccondry@cisco.com>
2018-04-18 15:22:46 -0500

    add node, express, body-parser await sql process, then return 201

--- index.js
+++ index.js
@@ -7,7 +7,7 @@ require('dotenv').load()
 app.use(cors())
 app.use(bodyParser.json({limit: '5kb'}))
 
-app.post('/', (req, res, next) {
+app.post('/', async (req, res, next) => {
   // post the incoming answers to the database
   // const data = {
   //   surveyId: 'SPARKY001',
@@ -16,11 +16,12 @@ app.post('/', (req, res, next) {
   //   q1: '4',
   //   q2: '5',
   // }
-  sql.saveAnswers(req.body)
+  await sql.saveAnswers(req.body)
+  return res.status(201).send()
 })
 
 // log 404
-app.use(function(req, res, next) {
+app.use((req, res, next) => {
   console.log('404 not found', req.url)
   next()
 })
