b5e03807026a7ebc5a3240af00e10985fdec8d32
Coty Condry <ccondry@cisco.com>
2018-07-27 01:43:30 -0500

    move callback to uccx folder =

--- src/routes/callback.js
+++ src/routes/callback.js
@@ -1,11 +1,11 @@
 const express = require('express')
 const router = express.Router()
-const cb = require('../models/callback.js')
+const uccxCallback = require('../models/uccx/callback.js')
 
 router.post('/', async (req, res) => {
   try {
     console.log('request for UCCX voice callback. request body:', req.body)
-    const rsp = await cb(req.body)
+    const rsp = await uccxCallback(req.body)
     return res.status(200).send(rsp)
   } catch (error) {
     console.log('failed to start UCCX callback', error.message)
