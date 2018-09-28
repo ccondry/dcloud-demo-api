cb407b8d07f2e41e4bf148efc878794fbfe57ef4
Coty Condry <ccondry@cisco.com>
2018-09-28 12:26:17 -0500

    allow GET to post survey answers

--- src/routes/survey.js
+++ src/routes/survey.js
@@ -25,4 +25,27 @@ router.post('/', async (req, res, next) => {
   }
 })
 
+// save survey answers to survey database
+router.get('/', async (req, res, next) => {
+  // example data
+  // const data = {
+  //   surveyId: 'SPARKY001',
+  //   ani: '1234567890',
+  //   name: 'Jim Smith',
+  //   q1: '4',
+  //   q2: '5',
+  // }
+  try {
+    console.log('request to save survey answers:', req.query)
+    // post the incoming answers to the database
+    const rows = await model.saveAnswers(req.query)
+    console.log('successfully saved survey answers')
+    // accepted
+    return res.status(201).send({rows})
+  } catch (e) {
+    console.error('failed to save survey answers', e)
+    return res.status(500).send(e)
+  }
+})
+
 module.exports = router
