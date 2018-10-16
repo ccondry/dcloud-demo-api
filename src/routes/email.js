a27648321e7c90bd1236095c06e798e4ac6934bd
Coty Condry <ccondry@cisco.com>
2018-10-16 13:46:46 -0500

    allow to email address to be specified

--- src/routes/email.js
+++ src/routes/email.js
@@ -3,22 +3,27 @@ const router = express.Router()
 const email = require('../models/email')
 
 router.post('/', async (req, res) => {
-  const mailOptions = {
-    from: `"${req.body.name || ''}" <${req.body.email}>`, // sender address
-    to: process.env.email_to, // list of receivers
-    subject: req.body.subject || '', // Subject line
-    text: req.body.text || '', // plain text body
-    html: req.body.html || '' // html body
-  }
   try {
+    // default TO address
+    let to = process.env.email_to
+    // try to make TO address from incoming data
+    if (req.body.to && req.body.to.length && req.indexOf('@') < 0) {
+      to = req.body.to + '@dcloud.cisco.com'
+    }
+    const mailOptions = {
+      from: `"${req.body.name || ''}" <${req.body.email}>`, // sender address
+      to, // list of receivers
+      subject: req.body.subject || '', // Subject line
+      text: req.body.text || '', // plain text body
+      html: req.body.html || '' // html body
+    }
     const info = await email.send(mailOptions)
     console.log(info)
     res.set("Content-Type", "application/json; charset=utf-8")
     .send(JSON.stringify(info, null, 2));
   } catch (error) {
-    console.log(error)
-    res.status(500)
-    .send(JSON.stringify(error, null, 2));
+    console.log('error sending email', req.body, ':', error.message)
+    res.status(500).send(error.message)
   }
 })
 
