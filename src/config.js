671ffd2c0bcdb5c84229429eb27c00956d6bb018
Coty Condry <ccondry@cisco.com>
2018-11-30 12:28:06 -0600

    swap health and utilities template contact ID

--- src/config.js
+++ src/config.js
@@ -1,7 +1,7 @@
 const histories = {
   'Finance': '6543211',
-  'Health': '6543212',
-  'Utilities': '6543213',
+  'Utilities': '6543212',
+  'Health': '6543213',
   'Travel': '6543214',
   'City': '6543215'
 }
