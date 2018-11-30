b3a15e8dd8396a646b1662502c75f5fe62020e2d
Coty Condry <ccondry@cisco.com>
2018-11-30 12:32:55 -0600

    try ben jones customer for travel

--- src/config.js
+++ src/config.js
@@ -2,7 +2,7 @@ const histories = {
   'Finance': '6543211',
   'Utilities': '6543212',
   'Healthcare': '6543213',
-  'Travel': '6543214',
+  'Travel': '1234564',
   'City': '6543215'
 }
 module.exports = {
