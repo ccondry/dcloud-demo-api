a524027632bc46a34b4a4e9ba889f7b0e1b0defa
Coty Condry <ccondry@cisco.com>
2018-12-05 12:08:36 -0600

    send to SFDC address if multichannel is set to 'sfdc'

--- src/routes/email.js
+++ src/routes/email.js
@@ -4,12 +4,20 @@ const email = require('../models/email')
 
 router.post('/', async (req, res) => {
   try {
-    // default TO address
-    let to = process.env.email_to
-    // try to make TO address from incoming data
-    if (req.body.to && req.body.to.length && req.body.to.indexOf('@') < 0) {
+    // determine TO email address
+    let to
+    if (req.body.multichannel === 'sfdc') {
+      // use SFDC email address?
+      to = process.env.SFDC_EMAIL
+    } else if (req.body.to && req.body.to.length && req.body.to.indexOf('@') < 0) {
+      // try to make TO address from incoming data
       to = req.body.to + '@dcloud.cisco.com'
+    } else {
+      // default TO address
+      to = process.env.email_to
     }
+
+    // construct nodemailer options
     const mailOptions = {
       from: `"${req.body.name || ''}" <${req.body.email}>`, // sender address
       to, // list of receivers
@@ -17,10 +25,11 @@ router.post('/', async (req, res) => {
       text: req.body.text || '', // plain text body
       html: req.body.html || '' // html body
     }
+    // send email
     const info = await email.send(mailOptions)
     console.log(info)
-    res.set("Content-Type", "application/json; charset=utf-8")
-    .send(JSON.stringify(info, null, 2));
+    // return response to client
+    return res.status(200).send(info)
   } catch (error) {
     console.log('error sending email', req.body, ':', error.message)
     res.status(500).send(error.message)
