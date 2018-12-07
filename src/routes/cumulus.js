9404c217ff75b050d8ac30536bf3993397c5f978
Coty Condry <ccondry@cisco.com>
2018-12-07 09:08:03 -0600

    if demo is PCCE, add config=true to the cumulus website URL so that the website knows to check demo session config for vertical selection instead of prompting with dialog box on the cumulus website

--- src/routes/cumulus.js
+++ src/routes/cumulus.js
@@ -10,8 +10,14 @@ router.get('/', async (req, res, next) => {
     // extract relevant data
     // determine if we should redirect to the secondary server or primary server
     const host = req.query.dev === 'true' ? process.env.MM_API_2 : process.env.MM_API_1
+    // construct the redirect URL
+    let redirect = `${host}?session=${json.session.id}&datacenter=${json.session.datacenter}`
+    // if PCCE, set the vertical config flag so that cumulus does not prompt for vertical selection
+    if (process.env.DEMO_TYPE === 'pcce') {
+      redirect += '&config=true'
+    }
     // redirect client with 302
-    return res.redirect(302, `${host}?session=${json.session.id}&datacenter=${json.session.datacenter}`)
+    return res.redirect(302, redirect)
   } catch (e) {
     console.error(e)
     return res.status(500).send(e.message)
