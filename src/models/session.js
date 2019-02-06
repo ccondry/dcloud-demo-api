a6a3b960e6fa01001bd01f1f50bb579d8236d01c
Coty Condry <ccondry@cisco.com>
2019-02-06 15:13:42 -0600

    add logging, and improve code with try/catch

--- src/models/session.js
+++ src/models/session.js
@@ -10,13 +10,23 @@ let session = {}
 const sessionFile = process.env.SESSION_XML_FILE || '/dcloud/session.xml'
 
 function readSessionFile () {
+  console.log('reading dCloud session information from file', sessionFile)
   // read the dcloud session file and return the contents of the DIDs section
   fs.readFile(sessionFile, function (err, data) {
-    if (err) return console.error(err)
-    // parse xml to json object
-    const json = JSON.parse(parser.toJson(data))
-    // extract the DIDs array
-    session = json.session
+    if (err) {
+      console.error('failed to read dCloud session information file', sessionFile, err)
+      return
+    }
+    try {
+      console.log('parsing dCloud session information file into JSON...')
+      // parse xml to json object
+      const json = JSON.parse(parser.toJson(data))
+      // extract the relevant info
+      session = json.session
+      console.log('successfully parsed dCloud session information file into JSON')
+    } catch (e) {
+      console.log('failed to parse dCloud session information file into JSON:', e)
+    }
   })
 }
 
