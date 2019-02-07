569d17216378f2892119a4e7055ab91ab21fd764
Coty Condry <ccondry@cisco.com>
2019-02-06 22:18:08 -0600

    add cumulus config to session info

--- src/routes/session.js
+++ src/routes/session.js
@@ -1,12 +1,23 @@
 const express = require('express')
 const router = express.Router()
 const session = require('../models/session')
+const cumulus = require('../models/cumulus')
 
 router.get('/', async (req, res, next) => {
   console.log('request to get session data')
   try {
-    // parse session.xml to json object
+    // get parsed session.xml as json object
     const json = await session.get()
+    // get local per-user config, if exists
+    try {
+      if (req.query.username) {
+        console.log('getting session configuration for', req.query.username)
+        const configuration = await cumulus.getConfig(req.query.username)
+        json.configuration = configuration
+      }
+    } catch (e) {
+      // continue
+    }
     // return data
     return res.status(200).send(json)
   } catch (e) {
