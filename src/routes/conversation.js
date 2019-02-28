efa308fc2ad403e31b435c61f2a99e613b0a218e
Coty Condry <ccondry@cisco.com>
2019-02-27 22:57:24 -0800

    validate input, return empty outputText in response if invalid

--- src/routes/conversation.js
+++ src/routes/conversation.js
@@ -7,6 +7,16 @@ const uuidv1 = require('uuid/v1')
 router.get('/', async (req, res) => {
   try {
     console.log('request for conversational IVR transcript AI')
+    // validate transcript query string exists, is not an empty string, and is
+    // not a string of only "+" characters
+    if (
+      !req.query ||
+      !req.query.transcript ||
+      !req.query.transcript.replace('+', '').length
+    ) {
+      // return empty result for invalid input
+      return res.status(200).send({outputText: ''})
+    }
     const rsp = await model({
       q: req.query.transcript,
       sessionId: req.query.sessionId,
