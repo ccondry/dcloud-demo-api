d46d2fdd1dfaf8ce08866cc07d873b10572cd665
Coty Condry <ccondry@cisco.com>
2019-06-04 16:45:20 -0500

    log result after returning response

--- src/routes/certification.js
+++ src/routes/certification.js
@@ -9,7 +9,9 @@ router.post('/', async (req, res) => {
     const grade = req.query.grade
     console.log('request to save precision queue certification data. agent =', agent, 'and grade =', grade)
     const rsp = await model(agent, grade)
-    return res.status(202).send(rsp)
+    res.status(202).send(rsp)
+    console.log('successfully saved precision queue certification data. agent =', agent, 'and grade =', grade)
+    return
   } catch (error) {
     console.log('failed to save precision queue certification data:', error.message)
     return res.status(500).send(error.message)
