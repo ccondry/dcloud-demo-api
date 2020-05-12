b27c6abbfc189cc477a570e84ef46e709574dfd0
Coty Condry <ccondry@cisco.com>
2020-05-12 16:39:21 -0500

    await the automation script, which should also catch its errors

--- src/routes/automation.js
+++ src/routes/automation.js
@@ -8,8 +8,8 @@ const exec = util.promisify(require('child_process').exec)
 router.get('/', async (req, res, next) => {
   try {
     console.log('request to run dCloud session automation script.')
-    exec('/opt/dcloud/automation.sh')
-    return res.status(200).send()
+    await exec('/opt/dcloud/automation.sh')
+    return res.status(200).send('Done! Your dCloud session information should now be available.')
   } catch (e) {
     // failed
     console.error('failed to get mobile app answers for', req.params.id, ':', e.message)
