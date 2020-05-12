7f4ed5c32df059dacf7bc95fd2596705cd6901b5
Coty Condry <ccondry@cisco.com>
2020-05-12 16:45:54 -0500

    try running node command directly?

--- src/routes/automation.js
+++ src/routes/automation.js
@@ -8,7 +8,7 @@ const exec = util.promisify(require('child_process').exec)
 router.get('/', async (req, res, next) => {
   try {
     console.log('request to run dCloud session automation script.')
-    await exec('/bin/bash /opt/dcloud/automation.sh')
+    await exec('cd /opt/dcloud-demo-automation;node src/index')
     return res.status(200).send('Done! Your dCloud session information should now be available.')
   } catch (e) {
     // failed
