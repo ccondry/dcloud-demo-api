f213ec5d21d9ffa72e57fee66e074545c23ee43d
Coty Condry <ccondry@cisco.com>
2022-03-17 13:49:02 -0500

    add user ID when forwarding user to branding website

--- src/routes/brand.js
+++ src/routes/brand.js
@@ -9,6 +9,7 @@ if (process.env.MM_BRAND_URL) {
   console.warn('process.env.MM_BRAND_URL is not configured. defaulting to', 'https://mm-brand.cxdemo.net')
   host = 'https://mm-brand.cxdemo.net'
 }
+
 // forward client to brand website
 router.get('/', async (req, res, next) => {
   try {
@@ -19,6 +20,11 @@ router.get('/', async (req, res, next) => {
     // const host = req.query.dev === 'true' ? process.env.MM_API_2 : process.env.MM_API_1
     // construct the redirect URL
     let redirect = `${host}/?session=${json.id}&datacenter=${json.datacenter}`
+
+    // copy user ID from request to redirect URL, if provided
+    if (req.query.userId) {
+      redirect += '&userId=' + req.query.userId
+    }
     // if PCCE, set the vertical config flag so that cumulus does not prompt for vertical selection
     // if (process.env.DEMO_TYPE === 'pcce') {
     //   redirect += '&config=true'
