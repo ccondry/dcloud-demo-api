3560128db5c8031a27fa8a7258d30d5bc9550e7e
Coty Condry <ccondry@cisco.com>
2022-04-07 14:48:07 -0500

    update URL and default org ID for CCAI getConfig function

--- src/models/ccai.js
+++ src/models/ccai.js
@@ -6,11 +6,11 @@ const agent = new https.Agent({
   rejectUnauthorized: false,
 })
 
-const orgId = process.env.CLOUD_CONNECT_ORG_ID || '47764486-9076-4b2c-97a2-03cc809066c6'
+const orgId = process.env.CLOUD_CONNECT_ORG_ID || '3bf70069-9149-4471-91eb-0dd2371ec2e2'
 
 // get cloud connect config info for given ID
 async function getConfig ({token, id}) {
-  const url = 'https://cms.appstaging.ciscoccservice.com/cms/api/auxiliary-data/resources/ccai-config/' + id
+  const url = 'https://config-gateway.produs1.ciscoccservice.com/cms/api/auxiliary-data/resources/ccai-config/' + id
   const options = {
     query: {
       scopes: 'cjp-ccai:read'
